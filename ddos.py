df4bb040ea5ed34bc232a0fab84181413c895b6b
KarthikB-dev <saberlight35@gmail.com>
2021-07-21 22:31:10 -0700

    Fix style with newline, add comment

--- ddos.py
+++ ddos.py
@@ -1,15 +1,18 @@
 from scapy.all import *
 
+# Generates packets to overwhelm your router
 def spam_router():
-    # router_mac = 
+    # router_mac =
     # TODO use ARP and grep to get the router's address
     e = Ether(router_mac)
-    t = TCP(flags = 'S')
+    t = TCP(flags="S")
     for i in range(10):
         send(e / IP() / t)
 
+
 def main():
     spam_router()
+
 
 if __name__ == "__main__":
     main()
